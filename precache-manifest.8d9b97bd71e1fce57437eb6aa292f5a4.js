f2c6c28e16ce7d99f76cc84388f8a798f9da9889
siva1005 <siva.krishna@impactanalytics.co>
2020-10-03 01:17:37 +0530

    Updates

---
+++ precache-manifest.8d9b97bd71e1fce57437eb6aa292f5a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340e20d0776c9594678d02f9fae65517",
+    "url": "/github.io/crown-stack-assignment/index.html"
+  },
+  {
+    "revision": "878acdf782e9a3796004",
+    "url": "/github.io/crown-stack-assignment/static/css/main.7838ff3c.chunk.css"
+  },
+  {
+    "revision": "a380788932be1f7cf0ce",
+    "url": "/github.io/crown-stack-assignment/static/js/2.7f62085f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/crown-stack-assignment/static/js/2.7f62085f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878acdf782e9a3796004",
+    "url": "/github.io/crown-stack-assignment/static/js/main.fccd59f9.chunk.js"
+  },
+  {
+    "revision": "65f6e7da0bf5f8c93451",
+    "url": "/github.io/crown-stack-assignment/static/js/runtime-main.dc3b4932.js"
+  }
+]);
